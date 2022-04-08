7def779656c41689dc268178f873f0bcfb1e2ed0
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2022-04-08 21:47:49 +0700

    Edited a landing page, login, and register

--- resources/js/Layouts/Guest.js
+++ resources/js/Layouts/Guest.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/inertia-react';
+import React from "react";
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link } from "@inertiajs/inertia-react";
+import { HomeIcon } from "@heroicons/react/outline";
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-zinc-900">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                    <HomeIcon className="w-20 h-20 text-violet-500" />
                 </Link>
             </div>
 
--- resources/js/Pages/Welcome2.jsx
+++ resources/js/Pages/Welcome2.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { Link, Head } from "@inertiajs/inertia-react";
+import Girl from "../../img/Girl_Arun.jpg";
 
 const navigation = [
     { name: "Product", href: "#" },
@@ -16,11 +17,11 @@ export default function Example() {
     return (
         <>
             <Head title="Welcome" />
-            <div className="relative bg-white overflow-hidden">
-                <div className="max-w-7xl mx-auto">
-                    <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+            <div className="relative bg-zinc-900 overflow-hidden min-h-screen">
+                <div className="max-w-7xl mx-auto ">
+                    <div className="relative z-10 pb-8 bg-zinc-900 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 lg:min-h-screen">
                         <svg
-                            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-violet-500 transform translate-x-1/2"
                             fill="currentColor"
                             viewBox="0 0 100 100"
                             preserveAspectRatio="none"
@@ -47,7 +48,7 @@ export default function Example() {
                                                 />
                                             </a>
                                             <div className="-mr-2 flex items-center md:hidden">
-                                                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                                <Popover.Button className="bg-zinc-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                                     <span className="sr-only">
                                                         Open main menu
                                                     </span>
@@ -61,20 +62,14 @@ export default function Example() {
                                     </div>
                                     <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
                                         {navigation.map((item) => (
-                                            <a
+                                            <Link
                                                 key={item.name}
                                                 href={item.href}
-                                                className="font-medium text-gray-500 hover:text-gray-900"
+                                                className="font-medium text-violet-500 hover:text-violet-300"
                                             >
                                                 {item.name}
-                                            </a>
+                                            </Link>
                                         ))}
-                                        <Link
-                                            href={route("login")}
-                                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                                        >
-                                            Log in
-                                        </Link>
                                     </div>
                                 </nav>
                             </div>
@@ -92,7 +87,7 @@ export default function Example() {
                                     focus
                                     className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
                                 >
-                                    <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                    <div className="rounded-lg shadow-md bg-zinc-900 ring-1 ring-black ring-opacity-5 overflow-hidden">
                                         <div className="px-5 pt-4 flex items-center justify-between">
                                             <div>
                                                 <img
@@ -102,7 +97,7 @@ export default function Example() {
                                                 />
                                             </div>
                                             <div className="-mr-2">
-                                                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                                <Popover.Button className="bg-zinc-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                                     <span className="sr-only">
                                                         Close main menu
                                                     </span>
@@ -118,18 +113,12 @@ export default function Example() {
                                                 <a
                                                     key={item.name}
                                                     href={item.href}
-                                                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                                                    className="block px-3 py-2 rounded-md text-base font-medium text-violet-500"
                                                 >
                                                     {item.name}
                                                 </a>
                                             ))}
                                         </div>
-                                        <Link
-                                            href={route("login")}
-                                            className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                                        >
-                                            Log in
-                                        </Link>
                                     </div>
                                 </Popover.Panel>
                             </Transition>
@@ -137,35 +126,28 @@ export default function Example() {
 
                         <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                             <div className="sm:text-center lg:text-left">
-                                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                    <span className="block xl:inline">
-                                        Data to enrich your
-                                    </span>{" "}
-                                    <span className="block text-indigo-600 xl:inline">
-                                        online business
-                                    </span>
+                                <h1 className="text-4xl tracking-tight font-extrabold text-violet-300 sm:text-5xl md:text-6xl">
+                                    Rainfran
                                 </h1>
-                                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                                    Anim aute id magna aliqua ad ad non deserunt
-                                    sunt. Qui irure qui lorem cupidatat commodo.
-                                    Elit sunt amet fugiat veniam occaecat fugiat
-                                    aliqua.
+                                <p className="mt-3 text-base text-violet-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                    Rainfran adalah suatu platform untuk
+                                    menuliskan artikel Anime dan Teknologi.
                                 </p>
                                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                     <div className="rounded-md shadow">
-                                        <a
-                                            href="#"
+                                        <Link
+                                            href={route("login")}
                                             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                                         >
-                                            Get started
-                                        </a>
+                                            It's Show Time
+                                        </Link>
                                     </div>
                                     <div className="mt-3 sm:mt-0 sm:ml-3">
                                         <a
-                                            href="#"
+                                            href={route("register")}
                                             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                                         >
-                                            Live demo
+                                            Membuat Identitas
                                         </a>
                                     </div>
                                 </div>
@@ -175,9 +157,9 @@ export default function Example() {
                 </div>
                 <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                     <img
-                        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-                        src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-                        alt=""
+                        className="h-56 w-full object-cover object-top sm:h-72 md:h-96 lg:w-full lg:h-full"
+                        src={Girl}
+                        alt="Cewek Anime"
                     />
                 </div>
             </div>
